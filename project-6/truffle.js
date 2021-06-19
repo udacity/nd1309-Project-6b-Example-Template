6a78c6b2d32e9fa1522ba55b890c5c5e8f4322e9
Marc Wissenbach <marc.wissenbach@cegeka.de>
2021-06-19 21:20:53 +0200

    added rinkeby setup and gitignore

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,3 +1,9 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const infuraKey = "a878b1e124f0461d9f7a68749475250f";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
@@ -5,6 +11,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  rinkeby: {
+    provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${inuraKey}`),
+    network_id: 4,
   },
   compilers: {
     solc: {
